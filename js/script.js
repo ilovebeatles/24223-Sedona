1b8b737ef8b01d7d8d158a755aba76106dae659e
Paviel <forcu2mblog@gmail.com>
2015-04-12 17:08:41 +0300

    One function update

--- js/script.js
+++ js/script.js
@@ -2,128 +2,37 @@
 
   var body = document.body;
 
-  // var target = body.querySelector(".travel-time").value; Value не может быть значение переменной?
-  // Можно ли сделать через одну функцию?
+// var target = body.querySelector(".travel-time").value; Value не может быть значение переменной?
 
+var travelTime = body.querySelector(".travel-time");
+var peoples = body.querySelector(".people-count");
+var childrens = body.querySelector(".children-count");
 
-  // function calculate() {
-  //   var plus = body.querySelector(".minus");
-  //   var minus = body.querySelector(".plus");
+function Counter(input, minVal, maxVal ) {
+  var parent = (input).parentNode;
+  var minus = parent.querySelector(".plus");
+  var plus = parent.querySelector(".minus");
 
-  //   plus.addEventListener("click", function(e) {
-  //     console.log('На кнопку нажали!');
-  //     e.preventDefault();
-  //     var days = this.parentNode;
-  //     console.log(days)
+  minus.addEventListener("click", function(e) {
+    e.preventDefault();
+    var target = input;
+    if(target.value >= minVal) {
+      target.value -= 1;
+    }
+  })
 
-  //     // Если внутри родителя есть дни
+  plus.addEventListener("click", function(e) {
+    e.preventDefault();
+    var target = input;
+    if(target.value <= maxVal) {
+      var countVal = parseInt(target.value) + 1;
+      target.value = +countVal
+    }
+  })
+}
 
-  //     if(days.querySelector(".travel-time")) {
-  //       var target = days.querySelector(".travel-time");
-  //       console.log(target);
-  //       console.log("Первое условие сработало!")
-
-  //       var countVal = parseInt(target.value) + 1;
-  //       target.value = +countVal
-  //     }
-
-  //     // Если внутри родителя есть количество взрослых
-
-  //     else if(days.querySelector(".people-count")) {
-  //       var target = days.querySelector(".people-count");
-  //       console.log(target);
-  //       console.log("Первое условие сработало!")
-
-  //       var countVal = parseInt(target.value) + 1;
-  //       target.value = +countVal
-  //     }
-
-
-  //   })
-
-  // }
-
-  // calculate();
-
-
-  function travelCounting() {
-
-    var parent = body.querySelector(".travel-time").parentNode;
-    var minus = parent.querySelector(".plus");
-    var plus = parent.querySelector(".minus");
-
-    minus.addEventListener("click", function(e) {
-    	e.preventDefault();
-    	var target = body.querySelector(".travel-time");
-    	if(target.value >= 2) {
-    		target.value -= 1;
-    	}
-    })
-
-    plus.addEventListener("click", function(e) {
-    	e.preventDefault();
-    	var target = body.querySelector(".travel-time");
-      if(target.value <= 29) {
-      	var countVal = parseInt(target.value) + 1;
-      	target.value = +countVal
-      }
-    })
-  }
-
-  function customerCounting() {
-
-    var parent = body.querySelector(".people-count").parentNode;
-    var minus = parent.querySelector(".plus");
-    var plus = parent.querySelector(".minus");
-
-    minus.addEventListener("click", function(e) {
-      e.preventDefault();
-      var parent = this.parentNode;
-      var target = parent.querySelector(".people-count");
-
-      if(target.value >= 2) {
-        target.value -= 1;
-      }
-    })
-
-    plus.addEventListener("click", function(e) {
-      e.preventDefault();
-      var target = body.querySelector(".people-count");
-      if(target.value <= 4) {
-        var countVal = parseInt(target.value) + 1;
-        target.value = +countVal
-      }
-    })
-  }
-
-  function childrenCounting() {
-
-    var parent = body.querySelector(".children-count").parentNode;
-    var minus = parent.querySelector(".plus");
-    var plus = parent.querySelector(".minus");
-
-    minus.addEventListener("click", function(e) {
-      e.preventDefault();
-      var parent = this.parentNode;
-      var target = parent.querySelector(".children-count");
-
-      if(target.value >= 1) {
-        target.value -= 1;
-      }
-    })
-
-    plus.addEventListener("click", function(e) {
-      e.preventDefault();
-      var target = body.querySelector(".children-count");
-      if(target.value <= 11) {
-        var countVal = parseInt(target.value) + 1;
-        target.value = +countVal
-      }
-    })
-  }
-
-  travelCounting();
-  customerCounting();
-  childrenCounting();
+Counter(travelTime, 2, 13);
+Counter(peoples, 2, 4);
+Counter(childrens, 1, 1);
 
 })();
