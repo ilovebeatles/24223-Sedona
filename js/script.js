e1edca4a6247dfb1cdf9e5d70cac8eaf52333109
Paviel <forcu2mblog@gmail.com>
2015-04-13 22:16:22 +0300

    Input update

--- js/script.js
+++ js/script.js
@@ -12,6 +12,45 @@ function Counter(input, minVal, maxVal ) {
   var parent = (input).parentNode;
   var minus = parent.querySelector(".plus");
   var plus = parent.querySelector(".minus");
+  var peopleContainer = body.querySelector(".row--parent");
+  var childContainer = body.querySelector(".row--children");
+
+  input.addEventListener("keyup", function() {
+
+    if (input == peoples) {
+
+      // Если вводимое значение меньше шести - удаляем всё и создаём заново нужное количество через цикл
+
+      if (input.value < 6) {
+        var rows = parent.querySelector(".row");
+        var parentInfo = body.querySelector("#parent-info").innerHTML;
+        peopleContainer.innerHTML = "";
+        for (i = 1; i < parseInt(input.value) + 1; i++) {
+          var html = Mustache.render(parentInfo, {
+              "parentCount": i
+            });
+          peopleContainer.innerHTML = peopleContainer.innerHTML + html
+        }
+      }
+    }
+
+    else if (input == childrens) {
+
+      // Если вводимое значение меньше трёх - удаляем всё и создаём зановон ужное количество через цикл
+
+      if (input.value < 3) {
+        var rows = parent.querySelector(".row");
+        var parentInfo = body.querySelector("#child-info").innerHTML;
+        childContainer.innerHTML = "";
+        for (i = 1; i < parseInt(input.value) + 1; i++) {
+          var html = Mustache.render(parentInfo, {
+              "childCount": i
+            });
+          childContainer.innerHTML = childContainer.innerHTML + html
+        }
+      }
+    }
+  })
 
   minus.addEventListener("click", function(e) {
     e.preventDefault();
@@ -22,17 +61,15 @@ function Counter(input, minVal, maxVal ) {
       // Если передаваемое значение - взрослые
 
       if (input == peoples) {
-        var container = body.querySelector(".row--parent");
-        var lastChild = container.children[target.value];
-        container.removeChild(lastChild);
+        var lastChild = peopleContainer.children[target.value];
+        peopleContainer.removeChild(lastChild);
       }
 
       // Если передаваемое значение - дети
 
       else if (input == childrens) {
-        var container = body.querySelector(".row--children");
-        var lastChild = container.children[target.value];
-        container.removeChild(lastChild);
+        var lastChild = childContainer.children[target.value];
+        childContainer.removeChild(lastChild);
       }
     }
   })
@@ -45,21 +82,19 @@ function Counter(input, minVal, maxVal ) {
       target.value = +countVal
 
       if (input == peoples) {
-        var container = body.querySelector(".row--parent");
         var parentInfo = body.querySelector("#parent-info").innerHTML;
         var html = Mustache.render(parentInfo, {
           "parentCount": input.value
         });
-        container.innerHTML = container.innerHTML + html;
+        peopleContainer.innerHTML = peopleContainer.innerHTML + html;
       }
 
       else if (input == childrens) {
-        var container = body.querySelector(".row--children");
         var parentInfo = body.querySelector("#child-info").innerHTML;
         var html = Mustache.render(parentInfo, {
           "childCount": input.value
         });
-        container.innerHTML = container.innerHTML + html;
+        childContainer.innerHTML = childContainer.innerHTML + html;
       }
     }
 
